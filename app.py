f5a442f6ee0e4650c12b4aef56375d724aad2d60
Ryan <kochy@hotmail.co.uk>
2023-07-24 12:08:13 +0100

    added cors

--- app.py
+++ app.py
@@ -2,27 +2,29 @@ from dotenv import load_dotenv
 from flask import Flask
 from models import *
 from controller import *
+from flask_cors import CORS
 
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/api/highscores", methods=["GET"])
 def get_api_highscores():
     response = get_highscores()
-    return response, 200
+    return response
 
 
 @app.route("/api/highscore", methods=["GET"])
 def get_api_highscore_query_username():
     response = get_highscore_query_username()
-    return response, 200
+    return response
 
 
 @app.route("/api/highscore", methods=["POST"])
 def get_api_post_sumbit_score():
     response = post_sumbit_score()
-    return response, 201
+    return response
 
 if __name__ == "__main__":
     app.run()
