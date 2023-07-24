d5b10db940d51e0f7902a367b81f8e814582ec7b
Ryan <kochy@hotmail.co.uk>
2023-07-24 12:01:14 +0100

    added status codes

--- app.py
+++ app.py
@@ -10,19 +10,19 @@ app = Flask(__name__)
 @app.route("/api/highscores", methods=["GET"])
 def get_api_highscores():
     response = get_highscores()
-    return response
+    return response, 200
 
 
 @app.route("/api/highscore", methods=["GET"])
 def get_api_highscore_query_username():
     response = get_highscore_query_username()
-    return response
+    return response, 200
 
 
 @app.route("/api/highscore", methods=["POST"])
 def get_api_post_sumbit_score():
     response = post_sumbit_score()
-    return response
+    return response, 201
 
 if __name__ == "__main__":
     app.run()
--- controller.py
+++ controller.py
@@ -4,16 +4,16 @@ from models import *
 
 def get_highscores():
     data = fetch_all_highscores()
-    return {"data": data}, 200
+    return {"data": data}
 
 def get_highscore_query_username():
     username = request.args.get("username")
     data = fetch_highscore_by_username(username)
-    return {"data": data}, 200
+    return {"data": data}
 
 def post_sumbit_score():
     post_data = request.get_json()
     username = post_data["username"]
     score = post_data["score"]
     data = insert_highscore(username, score)
-    return {"data": data}, 201
+    return {"data": data}
